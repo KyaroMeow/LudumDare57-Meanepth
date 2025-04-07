6336feb5f892147c0de562a5b89cfbc9d3ff7c6b
KyaroMeow <kirillid2006@gmail.com>
2025-04-07 14:03:55 +0300

    CorallsPrefab

---
+++ Meanepth-unity-3D/Assets/Scripts/Envierment/Megagandon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Megagandon : MonoBehaviour
+{
+    
+    void OnEnable()
+    {
+        
+    }
+
+   
+}
